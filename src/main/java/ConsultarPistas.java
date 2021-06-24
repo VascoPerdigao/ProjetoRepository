3aeffec36a009c67eeb9469a34c404508f1e0046
Vasco Perdigão <2192284@my.ipleiria.pt>
2021-06-24 22:42:55 +0100

    commit vasco final

--- src/main/java/ConsultarPistas.java
+++ src/main/java/ConsultarPistas.java
@@ -1,10 +1,29 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
-public class ConsultarPistas {
-    private JPanel panel1;
+public class ConsultarPistas extends JFrame{
+    private JPanel painelConsultarPista;
     private JComboBox comboBox1;
     private JComboBox comboBox2;
     private JComboBox comboBox3;
     private JTable table1;
     private JButton voltarAtrásButton;
+
+    public ConsultarPistas(){
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(painelConsultarPista);
+
+        voltarAtrásButton.addActionListener(this::voltarAtrásButtonPerformed);
+
+        pack();
+        setVisible(true);
+
+    }
+
+    public void voltarAtrásButtonPerformed(ActionEvent e) {
+        new janelaMegaEventos("Mega Eventos");
+        setVisible(false);
+
+    }
 }
--- src/main/java/ListaPaisesMedalhados.java
+++ src/main/java/ListaPaisesMedalhados.java
@@ -1,8 +1,28 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
-public class ListaPaisesMedalhados {
-    private JPanel panel1;
+public class ListaPaisesMedalhados extends JFrame{
+    private JPanel PaisesMedalhados;
     private JCheckBox checkBox1;
     private JTable table1;
     private JButton voltarAtrasButton;
+
+    public ListaPaisesMedalhados(){
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(PaisesMedalhados);
+
+        voltarAtrasButton.addActionListener(this::voltarAtrásButtonPerformed);
+
+
+        pack();
+        setVisible(true);
+
+    }
+
+    public void voltarAtrásButtonPerformed(ActionEvent e) {
+        new Estatistica();
+        dispose();
+
+    }
 }
