83e7463c4723406204cb47f9094845eabb981adf
Vasco Perdigão <2192284@my.ipleiria.pt>
2021-06-24 16:02:33 +0100

    janelas todas

---
+++ src/main/java/CEetapa.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class CEetapa {
+    private JPanel painelCEetapa;
+    private JComboBox comboBox1;
+    private JButton voltarAtrasButton;
+    private JButton criar_editarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JRadioButton mRadioButton;
+    private JRadioButton fRadioButton;
+    private JTextField textField4;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JLabel CEtitulo;
+}
