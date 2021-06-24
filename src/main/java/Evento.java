edf51e89c9d385bd82af3b2dbb6a1faa85b40e9f
José Pereira <2181328@my.ipleiria.pt>
2021-06-24 16:34:38 +0100

    Commit_V2

---
+++ src/main/java/Evento.java
@@ -0,0 +1,57 @@
+import java.util.LinkedList;
+
+public class Evento {
+    private String nome;
+    private Integer evento_ID = 0;
+    private Data dta_inicio, dta_fim;
+    private String Local, País;
+
+
+    public Evento(String nome, Integer evento_ID, Data dta_inicio, Data dta_fim, String local, String país) {
+        this.nome = nome;
+        this.evento_ID = evento_ID;
+        this.dta_inicio = dta_inicio;
+        this.dta_fim = dta_fim;
+        Local = local;
+        País = país;
+    }
+
+    public void editarEvento(String nome, Integer evento_ID, Data dta_inicio, Data dta_fim, String local, String país){
+        this.nome = nome;
+        this.evento_ID = evento_ID;
+        this.dta_inicio = dta_inicio;
+        this.dta_fim = dta_fim;
+        Local = local;
+        País = país;
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getEvento_ID() {
+        return evento_ID;
+    }
+
+    public Data getDta_inicio() {
+        return dta_inicio;
+    }
+
+    public Data getDta_fim() {
+        return dta_fim;
+    }
+
+    public String getLocal() {
+        return Local;
+    }
+
+    public String getPaís() {
+        return País;
+    }
+
+    public String getNomeEID() {
+        return String.valueOf(evento_ID).concat(" | ").concat(nome);
+    }
+}
